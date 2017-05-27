b963ddf2a7b896e0ed7acd0cb6e853b5ff100302
Den <godenis91@gmail.com>
2017-05-27 10:25:07 +0300

    lesson5

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,14 +13,28 @@ namespace Snake
         {
 
             Point p1 = new Point(1, 3, '*');          
-
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
 
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadLine();
+
         }
     }
 }
